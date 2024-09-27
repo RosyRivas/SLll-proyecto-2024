541a11a25d29336508c43eebc4729922fd2a4277
Diego Gonzalez <diego.gonzaaleez.a@gmail.com>
2024-09-27 09:49:13 -0300

    Se agrega ejemplo de PersonaDataBase

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SL2_proyecto.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,47 @@ namespace SL2_proyecto
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Persona nuevaPersona = new Persona
+                {
+                    Id = int.Parse(textBoxID.Text),
+                    Nombre = textBoxNombre.Text,
+                    Apellido = textBoxApellido.Text,
+                    FechaNacimiento = dateTimeFechaNac.Value,
+                    Sexo = Convert.ToChar(comboBoxSexo.Text)
+                };
+
+                // Crear una instancia de PersonaDataBase
+                PersonaDataBase personaDb = new PersonaDataBase();
+
+                // Llamar al método para insertar la nueva persona
+                personaDb.InsertarPersona(nuevaPersona);
+                MessageBox.Show("Persona insertada correctamente.");
+
+                //Limpiar controles
+                textBoxID.Clear();
+                textBoxNombre.Clear();
+                textBoxApellido.Clear();
+                dateTimeFechaNac.Value = DateTime.Now;
+                comboBoxSexo.Text = "";
+            }
+
+            catch (FormatException ex)
+            {
+                // Si ocurre un error de formato, por ejemplo al convertir texto en número
+                MessageBox.Show("Error de formato: " + ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                // Captura cualquier otro tipo de excepción que pueda ocurrir
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
         }
     }
 }
