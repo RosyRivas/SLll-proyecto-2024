026c983175cced798276afcb93c35e5f5eb4c3dd
Brian-1710 <braianduarte517@gmail.com>
2024-10-04 11:55:28 -0300

    Create Persona.cs
    creacion de clase persona

---
+++ Models/Persona.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Persona
+{
+	public int id { get; set; }
+	public string Nombre { get; set; }
+	public string Apellido { get; set; }
+	public DateTime FechaNacimiento { get; set; }
+	public char Sexo {  get; set; }
+}
