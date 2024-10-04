4b63761a185a69c706ca1219486b2854d1f59a8c
Rosa Rivas <33501967+RosyRivas@users.noreply.github.com>
2024-10-04 18:48:39 -0300

    Update Persona.cs

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -1,14 +1,4 @@
 ﻿using System;
-
-
-public class Persona
-{
-	public int id { get; set; }
-	public string Nombre { get; set; }
-	public string Apellido { get; set; }
-	public DateTime FechaNacimiento { get; set; }
-	public char Sexo {  get; set; }
-
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
