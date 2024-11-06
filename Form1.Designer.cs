4731927597c6175c20708b9537462e22db1f9e01
Camila <mendezcamilaitati@gmail.com>
2024-11-06 17:36:04 -0300

    se agrega la vista y configuracion de botones

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,59 +39,70 @@
             this.textBoxApellido = new System.Windows.Forms.TextBox();
             this.textBoxNombre = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnBaja = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(325, 240);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(278, 140);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 16);
+            this.label5.Size = new System.Drawing.Size(42, 16);
             this.label5.TabIndex = 22;
             this.label5.Text = "Sexo";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(231, 214);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(278, 93);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(132, 16);
+            this.label4.Size = new System.Drawing.Size(151, 16);
             this.label4.TabIndex = 21;
             this.label4.Text = "Fecha de nacimiento";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(306, 187);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 190);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.Size = new System.Drawing.Size(65, 16);
             this.label3.TabIndex = 20;
             this.label3.Text = "Apellido";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(307, 155);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 140);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.Size = new System.Drawing.Size(62, 16);
             this.label2.TabIndex = 19;
             this.label2.Text = "Nombre";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(343, 127);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 90);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.Size = new System.Drawing.Size(22, 16);
             this.label1.TabIndex = 18;
             this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBoxID
             // 
-            this.textBoxID.Location = new System.Drawing.Point(369, 124);
+            this.textBoxID.Location = new System.Drawing.Point(126, 90);
             this.textBoxID.MaxLength = 6;
             this.textBoxID.Name = "textBoxID";
-            this.textBoxID.Size = new System.Drawing.Size(55, 22);
+            this.textBoxID.Size = new System.Drawing.Size(117, 22);
             this.textBoxID.TabIndex = 17;
             // 
             // comboBoxSexo
@@ -100,49 +111,100 @@
             this.comboBoxSexo.Items.AddRange(new object[] {
             "F",
             "M"});
-            this.comboBoxSexo.Location = new System.Drawing.Point(369, 237);
+            this.comboBoxSexo.Location = new System.Drawing.Point(447, 137);
             this.comboBoxSexo.Name = "comboBoxSexo";
-            this.comboBoxSexo.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxSexo.Size = new System.Drawing.Size(200, 24);
             this.comboBoxSexo.TabIndex = 16;
             // 
             // dateTimeFechaNac
             // 
-            this.dateTimeFechaNac.Location = new System.Drawing.Point(369, 209);
+            this.dateTimeFechaNac.Location = new System.Drawing.Point(447, 90);
             this.dateTimeFechaNac.Name = "dateTimeFechaNac";
             this.dateTimeFechaNac.Size = new System.Drawing.Size(200, 22);
             this.dateTimeFechaNac.TabIndex = 15;
             // 
             // textBoxApellido
             // 
-            this.textBoxApellido.Location = new System.Drawing.Point(369, 184);
+            this.textBoxApellido.Location = new System.Drawing.Point(126, 190);
             this.textBoxApellido.MaxLength = 50;
             this.textBoxApellido.Name = "textBoxApellido";
-            this.textBoxApellido.Size = new System.Drawing.Size(100, 22);
+            this.textBoxApellido.Size = new System.Drawing.Size(117, 22);
             this.textBoxApellido.TabIndex = 14;
             // 
             // textBoxNombre
             // 
-            this.textBoxNombre.Location = new System.Drawing.Point(369, 152);
+            this.textBoxNombre.Location = new System.Drawing.Point(126, 140);
             this.textBoxNombre.MaxLength = 50;
             this.textBoxNombre.Name = "textBoxNombre";
-            this.textBoxNombre.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNombre.Size = new System.Drawing.Size(117, 22);
             this.textBoxNombre.TabIndex = 13;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(369, 267);
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(240, 234);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 59);
+            this.button1.Size = new System.Drawing.Size(200, 37);
             this.button1.TabIndex = 12;
             this.button1.Text = "Enviar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(266, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(152, 29);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "ABM simple";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(556, 302);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 37);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Modificar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnBaja
+            // 
+            this.btnBaja.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnBaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaja.Location = new System.Drawing.Point(556, 358);
+            this.btnBaja.Name = "btnBaja";
+            this.btnBaja.Size = new System.Drawing.Size(144, 37);
+            this.btnBaja.TabIndex = 26;
+            this.btnBaja.Text = "Eliminar";
+            this.btnBaja.UseVisualStyleBackColor = false;
+            this.btnBaja.Click += new System.EventHandler(this.btnBaja_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 302);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(486, 180);
+            this.dataGridView1.TabIndex = 24;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(712, 529);
+            this.Controls.Add(this.btnBaja);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -156,6 +218,8 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,6 +238,10 @@
         private System.Windows.Forms.TextBox textBoxApellido;
         private System.Windows.Forms.TextBox textBoxNombre;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnBaja;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,13 +13,21 @@ namespace SL2_proyecto
 {
     public partial class Form1 : Form
     {
+        // Crear una instancia de Persona
+        private PersonaDataBase PersonaController = new PersonaDataBase();
+        
         public Form1()
         {
+            // Inicializar el formulario
             InitializeComponent();
+            // Llenar el ComboBox con los valores "M" y "F"
+            comboBoxSexo.Items.Add("M");
+            comboBoxSexo.Items.Add("F");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Insertar una nueva persona
             try
             {
                 Persona nuevaPersona = new Persona
@@ -30,20 +38,17 @@ namespace SL2_proyecto
                     FechaNacimiento = dateTimeFechaNac.Value,
                     Sexo = Convert.ToChar(comboBoxSexo.Text)
                 };
-
-                // Crear una instancia de PersonaDataBase
-                PersonaDataBase personaDb = new PersonaDataBase();
+                
 
                 // Llamar al método para insertar la nueva persona
-                personaDb.InsertarPersona(nuevaPersona);
+                PersonaController.AgregarPersona(nuevaPersona);
                 MessageBox.Show("Persona insertada correctamente.");
 
                 //Limpiar controles
-                textBoxID.Clear();
-                textBoxNombre.Clear();
-                textBoxApellido.Clear();
-                dateTimeFechaNac.Value = DateTime.Now;
-                comboBoxSexo.Text = "";
+                ClearFields();
+
+                //Actualizar el listado de personas
+                ListarPersonas();
             }
 
             catch (FormatException ex)
@@ -58,5 +63,100 @@ namespace SL2_proyecto
                 MessageBox.Show("Ocurrió un error: " + ex.Message);
             }
         }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Cargar el listado al iniciar el formulario
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListarPersonas();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Modificacion de una persona existente
+            try
+            {
+                Persona personaModificada = new Persona
+                {
+                    Id = int.Parse(textBoxID.Text),
+                    Nombre = textBoxNombre.Text,
+                    Apellido = textBoxApellido.Text
+                };
+
+                PersonaController.ModificarPersona(personaModificada);
+                MessageBox.Show("Persona modificada correctamente.");
+
+                //limpiar los campos
+                ClearFields();
+
+                //listar las personas
+                ListarPersonas();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Error de formato: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al modificar: " + ex.Message);
+            }
+        }
+
+        private void btnBaja_Click(object sender, EventArgs e)
+        {
+            // Eliminar una persona por su ID
+            try
+            {
+                int id = int.Parse(textBoxID.Text);
+                PersonaController.EliminarPersona(id);
+                MessageBox.Show("Persona eliminada correctamente.");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Error de formato: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al eliminar: " + ex.Message);
+            }
+
+
+
+        }
+
+        //Listar las personas
+        private void ListarPersonas()
+        {
+            try
+            {
+                var personas = PersonaController.ObtenerPersonas();
+                dataGridView1.DataSource = personas;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener personas: " + ex.Message);
+            }
+        }
+
+        
+        private void ClearFields()
+        {
+            textBoxID.Clear();
+            textBoxNombre.Clear();
+            textBoxApellido.Clear();
+            dateTimeFechaNac.Value = DateTime.Now;
+            comboBoxSexo.Text = "";
+        }  
     }
 }
